19763824f1af9730de7c019e03c0a4f048bcc116
Marco De La Pierre <marco.delapierre@gmail.com>
2020-06-08 23:15:14 +0800

    s1: less of quay.io

--- exercises/pipeline/data/pull_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-singularity pull docker://quay.io/biocontainers/salmon:1.2.1--hf69c8f4_0
-singularity pull docker://quay.io/biocontainers/fastqc:0.11.9--0
-singularity pull docker://quay.io/biocontainers/multiqc:1.9--pyh9f0ad1d_0
