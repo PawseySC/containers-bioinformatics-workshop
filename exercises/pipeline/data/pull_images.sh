e38f3a53c4004b6a050da59ddc07feebb7a277fa
Marco De La Pierre <marco.delapierre@gmail.com>
2020-06-08 23:05:50 +0800

    s1: wrappers

--- exercises/pipeline/data/pull_images.sh
+++ exercises/pipeline/data/pull_images.sh
@@ -3,4 +3,3 @@
 singularity pull docker://quay.io/biocontainers/salmon:1.2.1--hf69c8f4_0
 singularity pull docker://quay.io/biocontainers/fastqc:0.11.9--0
 singularity pull docker://quay.io/biocontainers/multiqc:1.9--pyh9f0ad1d_0
-
