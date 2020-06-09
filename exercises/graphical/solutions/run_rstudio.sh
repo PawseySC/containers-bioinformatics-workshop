4c8bbe63400836b35fa042358fc902382078b7f2
Marco De La Pierre <marco.delapierre@gmail.com>
2020-06-09 16:11:09 +0800

    s2 : attempts 3 and 4

--- exercises/graphical/solutions/run_rstudio.sh
+++ exercises/graphical/solutions/run_rstudio.sh
@@ -2,12 +2,10 @@
 
 export SINGULARITYENV_USER=$USER
 export SINGULARITYENV_PASSWORD=rstudiopassword
-echo $SINGULARITYENV_USER && echo $SINGULARITYENV_PASSWORD
 
 singularity exec \
     -C \
-    -B home_rstudio:/home/rstudio \
-    -B home_rstudio:$HOME \
+    -B home_rstudio:/home/rstudio -B home_rstudio:$HOME \
     ggtree_2.0.4.sif \
     rserver --www-port 8787 --www-address 0.0.0.0 --auth-none=0 --auth-pam-helper-path=pam-helper
 
