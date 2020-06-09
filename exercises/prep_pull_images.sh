00c0c39285618c4f4afc335f79c0083258daee69
Marco De La Pierre <marco.delapierre@gmail.com>
2020-06-09 12:34:56 +0800

    s2: exercises files

--- exercises/prep_pull_images.sh
+++ exercises/prep_pull_images.sh
@@ -6,5 +6,8 @@ singularity exec docker://quay.io/biocontainers/fastqc:0.11.9--0 echo ciao
 singularity exec docker://quay.io/biocontainers/multiqc:1.9--pyh9f0ad1d_0 echo ciao
 
 # session 2
+singularity exec docker://marcodelapierre/ggtree:2.0.4 echo ciao
+singularity exec docker://marcodelapierre/ipycytoscape:0.2.2 echo ciao
+singularity exec docker://marcodelapierre/igv:2.8.3 echo ciao
 
 # session 3
