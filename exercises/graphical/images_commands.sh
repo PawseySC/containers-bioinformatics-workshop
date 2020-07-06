e461524af32e804ba3289aa4fb12080fc4c71bad
Marco De La Pierre <marco.delapierre@gmail.com>
2020-07-06 10:18:14 +0800

    fix to graphical instruction script

--- exercises/graphical/images_commands.sh
+++ exercises/graphical/images_commands.sh
@@ -5,8 +5,8 @@
 # command:  rserver --www-port 8787 --www-address 0.0.0.0 --auth-none=0 --auth-pam-helper-path=pam-helper
 
 # jupyter
-# image: 
-# command: 
+# image:  docker://marcodelapierre/ipycytoscape:0.2.2
+# command:  jupyter notebook --no-browser --port=8888 --ip 0.0.0.0 --notebook-dir=$HOME
 
 # igv
 # image:  docker://marcodelapierre/igv:2.8.3
